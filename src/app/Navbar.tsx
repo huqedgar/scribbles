383ee069df8f569cc307941551a0c3259a427f26
Hưng <96417124+huqedgar@users.noreply.github.com>
2023-06-26 18:58:47 +0700

    update logo

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,13 +1,21 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 
 export function Navbar() {
     return (
         <div className="border-b py-4">
-            <div className="container mx-auto flex justify-between">
-                <div>LOGO</div>
-                <nav className="flex gap-4">
+            <div className="container mx-auto flex justify-between items-center">
+                <div>
+                    <Image
+                        src={require('../../public/huqedgar.png')}
+                        alt="huqedgar"
+                        width={125}
+                        className="absolute -top-8 left-20"
+                    />
+                </div>
+                <nav className="flex gap-4 ml-8">
                     <Link href="/">Generate</Link>
                     <Link href="/collection">Collection</Link>
                 </nav>
